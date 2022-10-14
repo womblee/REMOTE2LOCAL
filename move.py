6cabf5b701d91e07851ac6a88d5926f6253313af
nloginov <52250786+womblee@users.noreply.github.com>
2022-10-14 21:37:41 +0200

    Add files via upload

---
+++ move.py
@@ -0,0 +1,54 @@
+# Imports
+import shutil
+from pathlib import Path
+from configparser import ConfigParser
+
+# Setup
+settings = ConfigParser()
+settings.read('settings.ini')
+
+# Data
+LocalDirectory = settings.get('Config', 'local') # Directory which will receive an update
+ParseDirectory = settings.get('Config', 'remote') # Directory from which to update
+
+# Validate
+if not Path(LocalDirectory).is_dir(): raise ValueError("Local directory not found!")
+if not Path(ParseDirectory).is_dir(): raise ValueError("Parse directory not found!")
+
+# Handler
+Handle = []
+
+# Fill
+for directory in Path(ParseDirectory).glob('*'):
+    # Validate
+    if directory.is_dir():
+        Handle.append(directory.name) # Figure out which ones we will need to delete and copy
+
+# Log
+Message = "Directories for copying: %d" % len(Handle)
+
+print(Message)
+
+# Actions
+for folder in Handle:
+    # Directories
+    directory = Path(LocalDirectory + "/" + folder)
+    remote = Path(ParseDirectory + "/" + folder)
+
+    # Delete
+    if directory.exists():
+        # Log
+        Message = "Deleting directory %s" % directory.name
+
+        print(Message)
+
+        # Remove
+        shutil.rmtree(directory)        
+
+    # Copy from remote folder to local
+    shutil.copytree(remote, directory)
+
+    # Log
+    Message = "Remote %s -> Local" % directory.name
+
+    print(Message)
